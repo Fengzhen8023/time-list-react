821b1d25836e84af2de0744df71e14733c5fbbce
Zhenzhen Feng <zfeng020@nam.ad.pwcinternal.com>
2019-10-11 11:35:22 +0800

    add register second step part.

--- src/components/registerFormPart/RegisterFormPart.jsx
+++ src/components/registerFormPart/RegisterFormPart.jsx
@@ -6,12 +6,28 @@ class RegisterFormPart extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            phone: "",
+            phone: "13659894548",
+            verificationCode: "",
+            password: "",
+            rePassword: "",
             phonePlaceholder: "手机号/邮箱",
+            verificationCodePlaceholder: "请输入验证码",
+            passwordPlaceholder: "输入6~16位登录密码",
+            rePasswordPlaceholder: "再次确认登录密码",
             phoneErrorMessage: "请输入你的手机号码",
+            verificationCodeErrorMessage: "请输入验证码",
+            passwordErrorMessage: "请输入密码",
+            rePasswordErrorMessage: "请再次确认登录密码",
+            isVerificationCodeErrorMessageShow: false,
+            isrePasswordErrorMessageShow: false,
+            isPasswordErrorMessageShow: false,
             isPhoneErrorMessageShow: false,
             isShakeAnimateShow: false,
-            isInputBackSpace: false
+            isVerificationCodeShakeAnimateShow: false,
+            isPasswordShakeAnimateShow: false,
+            isRePasswordShakeAnimateShow: false,
+            isInputBackSpace: false,
+            isAgreementChecked: true
         }
     }
 
@@ -19,30 +35,93 @@ class RegisterFormPart extends React.Component {
         return (
             <div className="register-main">
                 <div className="register-title">欢迎注册</div>
-                <div className={`info-form${this.state.isShakeAnimateShow ? " animated shake bounce fast" : ""}`}>
-                    <div className="phone-box">
-                        <input
-                            onFocus={() => this.changePlacehoder("phonePlaceholder", "")}
-                            onBlur={() => this.changePlacehoder("phonePlaceholder", "请输入手机号")}
-                            onKeyDown={(e) => this.handleKeyDown(e)}
-                            onChange={(e) => this.handlePhoneInput(e)}
-                            value={this.state.phone}
-                            placeholder={this.state.phonePlaceholder}
-                            type="text" className="phone" />
-                        <span className={`error-message${this.state.isPhoneErrorMessageShow ? "" : " hide-item"}`}>{this.state.phoneErrorMessage}</span>
+                <div className="register-step register-step-01 hide-item">
+                    <div className={`info-form${this.state.isShakeAnimateShow ? " animated shake bounce fast" : ""}`}>
+                        <div className="phone-box input-box">
+                            <input
+                                onFocus={() => this.changePlacehoder("phonePlaceholder", "")}
+                                onBlur={() => this.changePlacehoder("phonePlaceholder", "请输入手机号")}
+                                onKeyDown={(e) => this.handleKeyDown(e)}
+                                onChange={(e) => this.handlePhoneInput(e)}
+                                value={this.state.phone}
+                                placeholder={this.state.phonePlaceholder}
+                                type="text" className="phone" />
+                            <span className={`error-message${this.state.isPhoneErrorMessageShow ? "" : " hide-item"}`}>{this.state.phoneErrorMessage}</span>
+                        </div>
                     </div>
-                  
+                    <div className="register-option">
+                        <div className="return-option option-box">
+                            <Icon type="arrow-left" />
+                            <span>返回登陆</span>
+                        </div>
+                        <div
+                            onClick={() => this.isGoToRegister()}
+                            className="next-option option-box">
+                            <span>下一步</span>
+                            <Icon type="arrow-right" />
+                        </div>
+                    </div>
                 </div>
-                <div className="register-option">
-                    <div className="return-option option-box">
-                        <Icon type="arrow-left" />
-                        <span>返回登陆</span>
-                    </div>
-                    <div
-                        onClick={() => this.isGoToRegister()}
-                        className="next-option option-box">
-                        <span>下一步</span>
-                        <Icon type="arrow-right" />
+
+                <div className="register-step register-step-02">
+                    <div className="info-form">
+                        <p className="phone-number">+86 {this.state.phone}</p>
+                        <div className={`info-form varification-code-box input-box${this.state.isVerificationCodeShakeAnimateShow ? " animated shake bounce fast" : ""}`}>
+                            <input
+                                onFocus={() => this.changePlacehoder("verificationCodePlaceholder", "")}
+                                onBlur={() => this.changePlacehoder("verificationCodePlaceholder", "请输入验证码")}
+                                onKeyDown={(e) => this.handleKeyDown(e)}
+                                onChange={(e) => this.handleVerificationCodeInput(e)}
+                                value={this.state.verificationCode}
+                                placeholder={this.state.verificationCodePlaceholder}
+                                type="text" />
+                            <span className={`error-message${this.state.isVerificationCodeErrorMessageShow ? "" : " hide-item"}`}>{this.state.verificationCodeErrorMessage}</span>
+                        </div>
+                        <div className={`info-form password-box input-box${this.state.isPasswordShakeAnimateShow ? " animated shake bounce fast" : ""}`}>
+                            <input
+                                onFocus={() => this.changePlacehoder("passwordPlaceholder", "")}
+                                onBlur={() => this.changePlacehoder("passwordPlaceholder", "输入6~16位登录密码")}
+                                onKeyDown={(e) => this.handleKeyDown(e)}
+                                onChange={(e) => this.handlePasswordInput(e)}
+                                value={this.state.password}
+                                placeholder={this.state.passwordPlaceholder}
+                                type="password" />
+                            <span className={`error-message${this.state.isPasswordErrorMessageShow ? "" : " hide-item"}`}>{this.state.passwordErrorMessage}</span>
+                        </div>
+                        <div className={`info-form re-password-box input-box${this.state.isRePasswordShakeAnimateShow ? " animated shake bounce fast" : ""}`}>
+                            <input
+                                onFocus={() => this.changePlacehoder("rePasswordPlaceholder", "")}
+                                onBlur={() => this.changePlacehoder("rePasswordPlaceholder", "再次确认登录密码")}
+                                onKeyDown={(e) => this.handleKeyDown(e)}
+                                onChange={(e) => this.handleRePasswordInput(e)}
+                                value={this.state.rePassword}
+                                placeholder={this.state.rePasswordPlaceholder}
+                                type="password" />
+                            <span className={`error-message${this.state.isrePasswordErrorMessageShow ? "" : " hide-item"}`}>{this.state.rePasswordErrorMessage}</span>
+                        </div>
+
+                        <div className="agreement">
+                            <span className="out-line" onClick={this.changeAgreementCheckStatus}>
+                                <span 
+                                    className={`in-circle${this.state.isAgreementChecked ? "" : " hide-item"}`}>
+                                </span>
+                            </span>
+                            <span>我已认真阅读并同意</span>
+                            <a href="#">《注册协议》</a>
+                            <span className={`error-message${this.state.isAgreementChecked ? " hide-item" : ""}`}>请勾选</span>
+                        </div>
+
+                        <div className="register-option">
+                            <div className="return-option option-box">
+                                <Icon type="arrow-left" />
+                                <span>返回登陆</span>
+                            </div>
+                            <div
+                                onClick={() => this.confirmRegister()}
+                                className="next-option option-box">
+                                <span>确定</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -56,9 +135,19 @@ class RegisterFormPart extends React.Component {
             });
             return false;
         }
+        if (placeholderName === "verificationCodePlaceholder") {
+            this.setState({
+                "verificationCodePlaceholder": placeholderValue
+            });
+        }
         if (placeholderName === "passwordPlaceholder") {
             this.setState({
                 "passwordPlaceholder": placeholderValue
+            });
+        }
+        if (placeholderName === "rePasswordPlaceholder") {
+            this.setState({
+                "rePasswordPlaceholder": placeholderValue
             });
         }
     }
@@ -68,6 +157,33 @@ class RegisterFormPart extends React.Component {
         if(this.state.phone.length < 11 || this.state.isInputBackSpace) {
             this.setState({
                 phone: phoneInputValue.replace(/[^0-9]/g, "")
+            });
+        }
+    }
+
+    handleVerificationCodeInput = (e) => {
+        let verificationCodeInputValue = e.currentTarget.value;
+        if(this.state.verificationCode.length < 4 || this.state.isInputBackSpace) {
+            this.setState({
+                verificationCode: verificationCodeInputValue.replace(/[^a-zA-Z0-9]/g, "")
+            });
+        }
+    }
+
+    handlePasswordInput = (e) => {
+        let passwordInputValue = e.currentTarget.value;
+        if(this.state.password.length < 16 || this.state.isInputBackSpace) {
+            this.setState({
+                password: passwordInputValue
+            });
+        }
+    }
+
+    handleRePasswordInput = (e) => {
+        let rePasswordInputValue = e.currentTarget.value;
+        if(this.state.rePassword.length < 16 || this.state.isInputBackSpace) {
+            this.setState({
+                rePassword: rePasswordInputValue
             });
         }
     }
@@ -112,6 +228,35 @@ class RegisterFormPart extends React.Component {
                 isPhoneErrorMessageShow: true,
             });
         }, 100)
+    }
+
+    confirmRegister = () => {
+        console.log("确认注册");
+        console.log("验证码： ", this.state.verificationCode);
+        console.log("密码： ", this.state.password);
+        console.log("确认密码： ", this.state.rePassword);
+
+        /*
+            isVerificationCodeShakeAnimateShow: false,
+            isPasswordShakeAnimateShow: false,
+            isRePasswordShakeAnimateShow: false,
+
+            isVerificationCodeErrorMessageShow: false,
+            isrePasswordErrorMessageShow: false,
+            isPasswordErrorMessageShow: false,
+            
+            if(this.state.verificationCode.length < 4) {
+                this.setState({
+                    isVerificationCodeShakeAnimateShow: false
+                })
+            }
+       */
+    }
+
+    changeAgreementCheckStatus = () => {
+        this.setState((preState, props) => ({
+            isAgreementChecked: !preState.isAgreementChecked
+        }));
     }
 
     loginByQQ = () => {
