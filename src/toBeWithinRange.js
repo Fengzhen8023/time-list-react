b827478b1913f53949a2ffa9b3c65f3764478e38
Yevhen Kozlov <skyboyer@gmail.com>
2022-11-02 10:12:49 +0100

    added Jest config

---
+++ src/toBeWithinRange.js
@@ -0,0 +1,18 @@
+expect.extend({
+    toBeWithinRange(received, floor, ceiling) {
+      const pass = received >= floor && received <= ceiling;
+      if (pass) {
+        return {
+          message: () =>
+            `expected ${received} not to be within range ${floor} - ${ceiling}`,
+          pass: true,
+        };
+      } else {
+        return {
+          message: () =>
+            `expected ${received} to be within range ${floor} - ${ceiling}`,
+          pass: false,
+        };
+      }
+    },
+  });
